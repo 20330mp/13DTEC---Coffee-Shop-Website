ca43e3f05bdafdbf0c24c5dcbe48687e91682288
20330mp <125605135+20330mp@users.noreply.github.com>
2024-06-19 22:07:13 +0000

    V. 1.4 - Finished Header section

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-let navbar = document.querySelector(".navbar");
+let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
-  navbar.classList.toggle('active');
+    navbar.classList.toggle('active');
+    cartItem.classList.remove('active');
+    
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
 }
 
